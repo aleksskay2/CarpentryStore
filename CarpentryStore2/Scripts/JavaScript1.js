0ade960801222e6d6fba304349a7334c8c5465ca
User <User@WIN-C8OVOKMLI3T>
2017-03-06 16:55:17 +0300

    Добавьте файлы проекта.

---
+++ CarpentryStore2/Scripts/JavaScript1.js
@@ -0,0 +1,41 @@
+﻿//$(document).ready(function () 
+//     {
+//    $('#submenu').hide();
+
+//    $('#navbar').hover(
+//    function () {
+//        $('#submenu').show();
+//    }
+//, function () {
+//    $('#submenu').hide();
+//}
+//);
+//    });   //  конец события  double click 
+
+
+//$(document).ready(function () {
+//    $('html').dblclick(function () {
+
+//    });   //  конец события  double click 
+//});
+
+//$(document).ready(function () {
+//    $('a').mouseover(function(){
+//        var message = "<p>Вы навели указатель мыши на ссылку</p>";
+//        $('#categories').append(message);
+//    });
+//});
+
+//$(document).ready(function () {
+//    $('a').click(function () {
+//        var message = "<p> Это новая ссылка</p>";
+//        $('#categories').append(message);
+//    })
+//})
+
+//$(document).ready(function () {
+//    $('#firstButton').click(function () {
+//        $(this).val('Второй');
+//    })
+//})
+
